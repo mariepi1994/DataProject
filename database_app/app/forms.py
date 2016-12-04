73763fd9f2bae58d8c2357a5b61e99b78c557631
Marie <mariepi1994@gmail.com>
2016-12-04 15:26:30 -0500

    deleted old proj

--- database_app/app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class CountrySearch(Form):
-	country_name = StringField('country_name', validators=[DataRequired()])
--- database_app/config.py~
+++
@@ -1,6 +0,0 @@
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'somesuperdupersecretkey'
-
-SQLALCHEMY_DATABASE_URI = 'mysql://sql9147252:l3Nl6Me8sl@sql9.freemysqlhosting.net/sql9147252'
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False 
